8d476fdc2d878fb732cbdecb8c2fefb0c303ff97
Gonca Negis <gonca.negis@gmail.com>
2022-08-01 15:36:53 +0300

    added mongoose, connected database, add-post page will now add posts and render on the homepage

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const BlogPost = require('./models/BlogPost');
 
 const app = express();
 const PORT = 3001;
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/blog_posts');
 
 // Template engine
 app.set('view engine', 'ejs');
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await BlogPost.find({});
+  posts.map((post) => {
+    console.log(post.title);
+  });
+  res.render('index', { posts });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add-post', (req, res) => {
   res.render('add_post');
 });
-app.get('/post', (req, res) => {
-  res.render('post');
+app.post('/post', async (req, res) => {
+  await BlogPost.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
