0148837304e4c523e06f8e72fbe26439824c0b0f
Gonca Negis <gonca.negis@gmail.com>
2022-08-02 16:15:29 +0300

    added update, delete post functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const BlogPost = require('./models/BlogPost');
+const methodOverride = require('method-override');
+const pageController = require('./controllers/pageController');
+const postController = require('./controllers/postController');
 
 const app = express();
 const PORT = 3001;
@@ -15,28 +17,23 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // Routes
-app.get('/', async (req, res) => {
-  const posts = await BlogPost.find({});
-  res.render('index', { posts });
-});
+app.get('/', postController.getAllPosts);
+app.get('/post/:id', postController.getPost);
+app.get('/about', pageController.getAboutPage);
+app.get('/add-post', pageController.getAddPage);
+app.post('/post', postController.createPost);
+app.get('/post/edit/:id', pageController.getEditPage);
+app.put('/post/:id', postController.updatePost);
+app.delete('/post/:id', postController.deletePost);
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add-post', (req, res) => {
-  res.render('add_post');
-});
-app.post('/post', async (req, res) => {
-  await BlogPost.create(req.body);
-  res.redirect('/');
-});
-app.get('/post/:id', async (req, res) => {
-  const post = await BlogPost.findById(req.params.id);
-  res.render('post', { post });
-});
-
+// Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
